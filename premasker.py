fc6b06b4fda57b7bbb88899b728e13b10edd6b2c
Alex Martinez <amrt@milestone.dk>
2025-01-27 14:32:59 +0100

    fixed empty detection error (hopefully)

--- premasker.py
+++ premasker.py
@@ -1,4 +1,5 @@
-from modules.GroundingDINO.groundingdino.util.inference import load_model, load_image, predict, annotate
+from modules.GroundingDINO.groundingdino.util.inference import load_model, load_image, predict, annotate 
+# from groundingdino.util.inference import load_model, load_image, predict, annotate
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 import cv2 as cv
@@ -61,26 +62,44 @@ class GSAM_entity_masker():
         self.boxes_pixel = boxes_xyxy * self.axis_size
         
         self.sam_predictor.set_image(image=self.image_np)
-        self.masks, confs, low_res_masks = self.sam_predictor.predict(box=self.boxes_pixel, 
-                                                            multimask_output=False)
-        # ATM not saving masks, if we want to have several object with individual masks we need to act here
-        dsp_image = self.image_np
-        masked_img = np.zeros_like(dsp_image)
-        for mask_ in self.masks:
-            mask = mask_.squeeze(0)
-            mask_3ch = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
-            masked_img = np.where(mask_3ch>0, dsp_image, masked_img)
-        masked_img = cv.cvtColor(masked_img, cv.COLOR_BGR2RGB)
+        print("################### masks: ", self.boxes_pixel)
+        print("################### image: ", self.image_np.shape)
+        if len(self.boxes_pixel) > 0:
+            self.masks, confs, low_res_masks = self.sam_predictor.predict(box=self.boxes_pixel, 
+                                                                multimask_output=False)
+            # ATM not saving masks, if we want to have several object with individual masks we need to act here
+            dsp_image = self.image_np
+            masked_img = np.zeros_like(dsp_image)
+            for mask_ in self.masks:
+                mask = mask_.squeeze(0)
+                mask_3ch = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
+                masked_img = np.where(mask_3ch>0, dsp_image, masked_img)
+            masked_img = cv.cvtColor(masked_img, cv.COLOR_BGR2RGB)
 
-        #add full out dir path when finish testing 
-        masked_img_path = self.out_dir + "/" + "premasked_" + self.image_path.split("/")[-1]
-        cv.imwrite(masked_img_path, masked_img)
-        
-        return masked_img_path
+            #add full out dir path when finish testing 
+            masked_img_path = self.out_dir + "/" + "premasked_" + self.image_path.split("/")[-1]
+            cv.imwrite(masked_img_path, masked_img)
+            
+            return masked_img_path
+        else:
+            return self.image_path
 
 if __name__ == '__main__':
-    image_path = "../../data/test_images/00286.jpg"
-    text = "car . person . bus . door . box . cart ."
-    masker = GSAM_entity_masker(image_path, text)
+    image_path = "../../data/test_images/blue_woman.jpg"
+    text = "car . person . woman"
+    out_dir = "../../data/out/"
+    gdino_checkpoint = "../GroundingDINO/weights/groundingdino_swint_ogc.pth"
+    gdino_model_cfg = "../GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+    sam_checkpoint = "checkpoints/sam2.1_hiera_large.pt"
+    sam_model_cfg = "sam2/sam2.1_hiera_l.yaml"
+    sam_config_dir_path = "../sam2/"
+    masker = GSAM_entity_masker(out_dir = out_dir, 
+                                gdino_checkpoint=gdino_checkpoint,
+                                gdino_model_cfg=gdino_model_cfg,
+                                sam_checkpoint=sam_checkpoint,
+                                sam_config_dir=sam_config_dir_path,
+                                sam_model_cfg=sam_model_cfg,
+                                image_path= image_path,
+                                 text= text)
     new_path = masker.mask()
     print(new_path)
